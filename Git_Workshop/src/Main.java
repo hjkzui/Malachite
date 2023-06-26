fc53664c5b7d139063ebc0fe481b5c102da220ef
Karoly <big.karlitosz@gmail.com>
2023-06-26 13:38:48 +0200

    b1

--- Git_Workshop/src/Main.java
+++ Git_Workshop/src/Main.java
@@ -2,4 +2,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    public void count(){
+        System.out.printf("23");
+    }
+
 }
